58130c94f2b69b1eeddffa737f885af5cdee1868
clintedward <clintedward86@icloud.com>
2019-04-11 12:13:20 +0200

    Updates

---
+++ precache-manifest.ced520202bcdb7368b7e4c3042ae3d6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "976c1f71d96c3a620800",
+    "url": "/static/js/main.976c1f71.chunk.js"
+  },
+  {
+    "revision": "b3d657e12371133225cd",
+    "url": "/static/js/2.b3d657e1.chunk.js"
+  },
+  {
+    "revision": "976c1f71d96c3a620800",
+    "url": "/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "b3d657e12371133225cd",
+    "url": "/static/css/2.c7c06a3d.chunk.css"
+  },
+  {
+    "revision": "538932de02281227a7325f1d5f78d8ab",
+    "url": "/index.html"
+  }
+];
